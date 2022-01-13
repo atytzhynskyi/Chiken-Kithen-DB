84c303f7fea7519b4e5f7eab5aebe3b6d34f76a6
atytzhynskyi <atytzhynskyi@future-processing.com>
2022-01-13 16:24:41 +0200

    6.10.3(unfinished)

--- CommandsModule/Commands/Table.cs
+++ CommandsModule/Commands/Table.cs
@@ -16,7 +16,7 @@ namespace CommandsModule
         private Accounting accounting { get; set; }
         private Kitchen kitchen { get; set; }
         private Hall hall { get; set; }
-
+        private bool _isPooled;
         public List<Buy> Buys = new List<Buy>();
 
         List<Customer> _Customers = new List<Customer>();
@@ -31,14 +31,14 @@ namespace CommandsModule
             FullCommand = _FullCommand;
             CommandType = FullCommand.Split(", ")[0];
             IsAllowed = false;
-
-            foreach (var foodName in GetFoodsFromCommand())
+            _isPooled = FullCommand.Split(", ")[1] == "Pooled";
+            foreach (var foodName in GetFoodsNameFromCommand())
             {
                 Food searchFood = kitchen.Storage.GetRecipe(foodName);
                 if (!object.Equals(searchFood, null)) _Orders.Add(searchFood);
             }
 
-            foreach (var customerName in GetCustomersFromCommand())
+            foreach (var customerName in GetCustomersNameFromCommand())
             {
                 Customer searchCustomer = hall.GetCustomer(customerName);
                 if (!object.Equals(searchCustomer, null)) _Customers.Add(searchCustomer);
@@ -59,6 +59,13 @@ namespace CommandsModule
 
             Buys = FormBuysFromCommand();
 
+            if (_isPooled && _Customers.Any(c => c.budget < accounting.CalculateFoodMenuPrice(
+                                                      kitchen.Storage.Recipes, c.Order)))
+            {
+                ExecuteTablePooled();
+                return;
+            }
+
             double startBudget = accounting.Budget;
             foreach (var buy in Buys)
             {
@@ -66,11 +73,94 @@ namespace CommandsModule
             }
 
             double moneyAmount = Math.Round(accounting.Budget - startBudget, 2);
-            double tax = Math.Round(moneyAmount * accounting.transactionTax,2);
-            Result = $"success; money amount: {moneyAmount}; tax:{tax}\n"+'{';
+            double tax = Math.Round(moneyAmount * accounting.transactionTax, 2);
+            Result = $"success; money amount: {moneyAmount}; tax:{tax}\n" + '{';
 
             Buys.ForEach(b => Result += $"\n\t{b.FullCommand} -> {b.Result}");
             Result += "\n}";
+        }
+        private double _budgetPool = 0;
+        private Dictionary<Customer, double> _donatedForTips = new Dictionary<Customer, double>();
+        private void ExecuteTablePooled()
+        {
+            _Customers.ForEach(c => _donatedForTips.Add(c, 0));
+
+            FillBudgetPool();
+            AddMoneyForTips();
+            foreach (var buy in Buys)
+            {
+                var price = accounting.CalculateFoodMenuPrice(kitchen.Storage.Recipes, buy.Food);
+                if(price > buy.Customer.budget)
+                {
+                    buy.Customer.budget = Math.Round(price - buy.Customer.budget, 2);
+                }
+            }
+
+        }
+
+        private void AddMoneyForTips()
+        {
+            var budgetPoolNeeded = 0.0;
+            _Orders.ForEach(o => budgetPoolNeeded += accounting.CalculateFoodMenuPrice(kitchen.Storage.Recipes, o));
+            var maxTipsAmount = budgetPoolNeeded * accounting.maxTipPercent;
+
+            var customersWithMoney = _Customers.Where(c => c.budget > 0).ToList();
+
+            while (customersWithMoney.Count > 0 && maxTipsAmount > _donatedForTips.Values.Sum())
+            {
+                customersWithMoney = _Customers.Where(c => c.budget > 0).ToList();
+
+                Customer pureCustomer = new Customer();
+                double minBudget = int.MaxValue;
+                foreach (var customer in _Customers)
+                {
+                    if (minBudget >= customer.budget)
+                    {
+                        pureCustomer = customer;
+                        minBudget = customer.budget;
+                    }
+                }
+
+                var i = maxTipsAmount - _donatedForTips.Values.Sum() / customersWithMoney.Count > pureCustomer.budget ? 
+                                                                                                        pureCustomer.budget : maxTipsAmount - _donatedForTips.Values.Sum() / customersWithMoney.Count;
+                foreach(var customer in customersWithMoney)
+                {
+                    _donatedForTips[customer] = Math.Round(_donatedForTips[customer] + i, 2);
+                    customer.budget = Math.Round(customer.budget - i, 2);
+                }
+            }
+        }
+
+        private void FillBudgetPool()
+        {
+            double budgetPoolNeeded = 0;
+            _Orders.ForEach(o => budgetPoolNeeded += accounting.CalculateFoodMenuPrice(kitchen.Storage.Recipes, o));//calculate budgetPoolNeeded
+
+            var customersWithMoney = _Customers.Where(c => c.budget > 0).ToList(); //get customers with money
+            while (budgetPoolNeeded != _budgetPool && customersWithMoney.Count > 0)
+            {
+                customersWithMoney = _Customers.Where(c => c.budget > 0).ToList(); 
+
+                //find the purest customers
+                Customer pureCustomer = new Customer();
+                double minBudget = int.MaxValue;
+                foreach (var customer in _Customers)
+                {
+                    if (minBudget >= customer.budget)
+                    {
+                        pureCustomer = customer;
+                        minBudget = customer.budget;
+                    }
+                }
+                  
+                //how much money we would take at this iteration?
+                var i = (budgetPoolNeeded - _budgetPool) / customersWithMoney.Count > pureCustomer.budget ? 
+                                                                                        pureCustomer.budget : (budgetPoolNeeded - _budgetPool) / customersWithMoney.Count;
+
+                //pool money from customers budgets to pool
+                _budgetPool = Math.Round(_budgetPool + (i * customersWithMoney.Count) , 2);
+                customersWithMoney.ForEach(c => c.budget = Math.Round(c.budget - i, 2));
+            }
         }
 
         private void SetCustomersOrders()
@@ -104,13 +194,13 @@ namespace CommandsModule
             }
             return buys;
         }
-        private List<string> GetCustomersFromCommand()
+        private List<string> GetCustomersNameFromCommand()
         {
             List<string> commandSplit = new List<string>(FullCommand.Split(", "));
             List<string> customersName = (List<string>)commandSplit.Where(s => !object.Equals(hall.GetCustomer(s), null)).ToList();
             return customersName;
         }
-        private List<string> GetFoodsFromCommand()
+        private List<string> GetFoodsNameFromCommand()
         {
             List<string> commandSplit = new List<string>(FullCommand.Split(", "));
             List<string> foodsName = commandSplit.Where(s => !object.Equals(kitchen.Storage.GetRecipe(s), null)).ToList();
@@ -148,16 +238,17 @@ namespace CommandsModule
             }
 
             if (_Customers.Any(c => c.budget < accounting.CalculateFoodMenuPrice(
-                                                         kitchen.Storage.Recipes, c.Order)))
+                                                         kitchen.Storage.Recipes, c.Order)) && !_isPooled)
             {
                 Result = "FAIL. One or more persons dont have enough money";
                 return;
             }
+
         }
 
         private bool IsEnoughIngredients()
         {
-            List<string> foodsName = GetFoodsFromCommand();
+            List<string> foodsName = GetFoodsNameFromCommand();
 
             //form MEGAfood recipe which contain every recipes of foods in orders
             Food megaFood = new Food("");
